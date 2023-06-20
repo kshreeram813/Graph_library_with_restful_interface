290208142dc69c9ff7fb85ffc144112c99340599
SHREERAM KUMAR KUSHWAHA <117209741+kshreeram813@users.noreply.github.com>
2023-06-20 11:02:07 +0530

    app.py

--- app.py
+++ app.py
@@ -9,36 +9,28 @@ def index():
 
 class Graph:
     def __init__(self):
-        self.edges = defaultdict(list)
-        self.weights = {}
+        self.edges = defaultdict(list)    # A dictionary to store the edges of the graph
+        self.weights = {}  # A dictionary to store the weights of the edges
 
-    def add_edge(self, from_node, to_node, weight):
+    def add_edge(self, from_node, to_node, weight):     # The code iterates over the edges and adds them to the graph
         self.edges[from_node].append(to_node)
         self.edges[to_node].append(from_node)
         self.weights[(from_node, to_node)] = weight
         self.weights[(to_node, from_node)] = weight
 
-# Create the graph and add edges
-graph = Graph()
+graph = Graph()     # Create the graph and add edges
 edges = [
-    ('X', 'A', 7),
-    ('X', 'B', 2),
-    ('X', 'C', 3),
-    ('X', 'E', 4),
-    ('A', 'B', 3),
-    ('A', 'D', 4),
-    ('B', 'D', 4),
-    ('B', 'H', 5),
-    ('C', 'L', 2),
-    ('D', 'F', 1),
-    ('F', 'H', 3),
-    ('G', 'H', 2),
-    ('G', 'Y', 2),
-    ('I', 'J', 6),
-    ('I', 'K', 4),
-    ('I', 'L', 4),
-    ('J', 'L', 1),
-    ('K', 'Y', 5),
+    ('A', 'B', 1),
+    ('A', 'C', 2),
+    ('B', 'F', 1),
+    ('F', 'E', 1),
+    ('E', 'D', 1),
+    ('D', 'C', 2),
+    ('B', 'D', 3),
+    ('C', 'E', 2),
+    ('C', 'F', 4),
+    ('D', 'F', 2),
+    ('E', 'A', 3),
 ]
 
 for edge in edges:
@@ -47,6 +39,7 @@ for edge in edges:
 @app.route('/shortest_path', methods=['POST', 'GET'])
 def shortest_path():
     if request.method == 'POST':
+        # If the request method is POST, retrieve the source and destination values from the form
         source = request.form['source']
         destination = request.form['destination']
         print("Received POST request")
@@ -54,64 +47,89 @@ def shortest_path():
         print("Destination:", destination)
 
     elif request.method == 'GET':
+        # If the request method is GET, retrieve the source and destination values from the query parameters
         source = request.args.get('source')
         destination = request.args.get('destination')
         print("Received GET request")
         print("Source:", source)
         print("Destination:", destination)
     else:
+        # If the request method is neither POST nor GET, return an error message
         return {"Error": "Invalid request method"}
 
-    if source.upper() not in graph.edges:
+    if source.upper() not in graph.edges:    # Check if source node is available in the graph
         return render_template('result.html', result={"Message": f"Source '{source}' is not available."})
 
-    if destination.upper() not in graph.edges:
+    if destination.upper() not in graph.edges:   # Check if destination node is available in the graph
         return render_template('result.html', result={"Message": f"Destination '{destination}' is not available."})
 
-    path, distance = dijsktra(graph, source.upper(), destination.upper())
-    if path == "Route Not Possible":
+    path, distance = dijsktra(graph, source.upper(), destination.upper()) # Find the shortest path and distance using Dijkstra's algorithm
+    if path == "Route Not Possible":    # If the path is "Route Not Possible", display an error message
         return render_template('result.html', result={"Message": "Route not possible"})
     else:
-        return render_template('result.html', result={"Path": "=>".join(path), "Distance": distance})
+        # dictionary is created to store the result of the shortest path calculation
+        result_data = {"Path": "=>".join(path), "Distance": distance}  
+        store_result(result_data)
+        return render_template('result.html', result=result_data)
 
 def dijsktra(graph, initial, end):
     shortest_paths = {initial: (None, 0)}
     current_node = initial
     visited = set()
 
-    while current_node != end:
-        visited.add(current_node)
+    while current_node != end:         # Continue until the current_node becomes the end node
+        visited.add(current_node)    # Mark the current_node as visited
         destinations = graph.edges[current_node]
         weight_to_current_node = shortest_paths[current_node][1]
 
-        for next_node in destinations:
+        for next_node in destinations:  # Iterate over the neighboring nodes
             weight = graph.weights[(current_node, next_node)] + weight_to_current_node
-            if next_node not in shortest_paths:
+            if next_node not in shortest_paths:  # If next_node is not in shortest_paths, add it
                 shortest_paths[next_node] = (current_node, weight)
             else:
                 current_shortest_weight = shortest_paths[next_node][1]
-                if current_shortest_weight > weight:
+                if current_shortest_weight > weight:    # If the new weight is smaller, update the shortest_paths dictionary
                     shortest_paths[next_node] = (current_node, weight)
 
         next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
-        if not next_destinations:
+        if not next_destinations:   # If there are no unvisited nodes, the route is not possible
             return "Route Not Possible"
-
+        # Find the next node with the minimum weight and update the current_node
         current_node = min(next_destinations, key=lambda k: next_destinations[k][1])
 
-    path = []
-    distance = shortest_paths[end][1]
+    path = []    # Reconstruct the path from end to start
+    distance = shortest_paths[end][1]     # swap method
     while current_node is not None:
         path.append(current_node)
         next_node = shortest_paths[current_node][0]
         current_node = next_node
 
-    path = path[::-1]
+    path = path[::-1]    # Reverse the path to start from the source node
     return path, distance
 
+def store_result(data):     # Writes the path and distance data to a text file named 'result.txt'.
+    with open('result.txt', 'w') as file:
+        file.write(f"Path: {data['Path']}\n")
+        file.write(f"Distance: {data['Distance']}")
+
+def read_result():   # Reads the path and distance data from the 'result.txt' file and returns it as a dictionary.
+    with open('result.txt', 'r') as file:
+        path = file.readline().split(": ")[1].strip()
+        distance = file.readline().split(": ")[1].strip()
+    return {"Path": path, "Distance": distance}
+
+@app.route('/display_result')
+def display_result():
+    result_data = read_result()
+    return render_template('result.html', result=result_data)   # pass- data to reult.html page
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(debug=True, host='2409:40f2:2e:71b3:b8ad:172e:4ac5:43cb', port=5000)
 
 
 
-    
+
+
+
+
